2843f4cf03995d15e004269107afdff1262c92c0
koujyungenn <513137767@qq.com>
2022-01-19 14:27:12 +0800

    类加载器-初始化过程笔记与单元代码

---
+++ com.kjg/src/code/zeroTwo.java
@@ -0,0 +1,58 @@
+package src.code;
+
+class Father {
+
+}
+
+public class zeroTwo extends Father{
+    private int a = -1;
+
+    public zeroTwo(){
+        a = 100;
+    }
+
+
+    static {
+        str = "def";
+//        System.out.println(str);
+    }
+
+    public static String str = "abc";
+
+
+    public static void main(String[] args) {
+        System.out.println(str);
+
+        // test clinit lock
+        new Thread(()->{
+            String name = Thread.currentThread().getName();
+            System.out.println(String.format("%s start clinit",name));
+            LockClass lockClass = new LockClass();
+            System.out.println(String.format("%s end clinit",name));
+        }).start();
+
+        new Thread(()->{
+            String name = Thread.currentThread().getName();
+            System.out.println(String.format("%s start clinit",name));
+            LockClass lockClass = new LockClass();
+            System.out.println(String.format("%s end clinit",name));
+        }).start();
+
+
+    }
+}
+
+class LockClass{
+
+    public static  boolean lock = false;
+
+    static{
+        String threadName = Thread.currentThread().getName();
+        System.out.println(String.format("%s线程正在执行LockClass的clinit()",threadName));
+        if(lock) {
+            while (true) {
+
+            }
+        }
+    }
+}
