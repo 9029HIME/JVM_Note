34acdfbb2d8323cdd369bff277e0a37f2c8d8c46
koujyungenn <513137767@qq.com>
2022-01-24 13:36:13 +0800

    PC寄存器

---
+++ com.kjg/src/code/PcCounter.java
@@ -0,0 +1,13 @@
+package src.code;
+
+public class PcCounter {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 11;
+        int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
