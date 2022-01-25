0c7b2259ef8688094ffb23fbb0cbfd6609440e02
koujyungenn <513137767@qq.com>
2022-01-25 13:37:51 +0800

    JVM栈初步记录

---
+++ com.kjg/src/code/JvmStack.java
@@ -0,0 +1,17 @@
+package src.code;
+
+public class JvmStack {
+    public static void main(String[] args) {
+        JvmStack jvmStack = new JvmStack();
+        jvmStack.methodA();
+    }
+
+    public void methodA(){
+        int i = 10;
+        int j = 20;
+    }
+
+    public void methodB(){
+        int k = 30;
+    }
+}
