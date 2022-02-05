c3e173ebdf265794fb376a420355650eae01926f
koujyungenn <513137767@qq.com>
2022-02-05 21:50:21 +0800

    局部变量表，准备细讲slot

---
+++ com.kjg/src/code/LocalVariables.java
@@ -0,0 +1,10 @@
+package src.code;
+
+public class LocalVariables {
+    public static void main(String[] args) {
+        int i = 123;
+        LocalVariables localVariables = new LocalVariables();
+        double d = (double) 23.2342;
+        int j = 456;
+    }
+}
