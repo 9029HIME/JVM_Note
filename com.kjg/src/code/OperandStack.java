dbbb71d2b5c975f8af526e8b1c07b3399824171e
koujyungenn <513137767@qq.com>
2022-02-06 14:31:47 +0800

    操作数栈

---
+++ com.kjg/src/code/OperandStack.java
@@ -0,0 +1,16 @@
+package src.code;
+
+public class OperandStack {
+    public static void main(String[] args) {
+
+    }
+    public void add() {
+        int i = getSum();
+        int j = 8;
+        int k = i + j;
+    }
+
+    public int getSum() {
+        return 6;
+    }
+}
