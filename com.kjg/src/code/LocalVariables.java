3c0f8259e09477305a88d31d4aa7c38c3315e57c
koujyungenn <513137767@qq.com>
2022-02-06 12:02:18 +0800

    slot结束，准备看操作数栈

--- com.kjg/src/code/LocalVariables.java
+++ com.kjg/src/code/LocalVariables.java
@@ -6,5 +6,19 @@ public class LocalVariables {
         LocalVariables localVariables = new LocalVariables();
         double d = (double) 23.2342;
         int j = 456;
+        localVariables.testThis(999);
+    }
+
+    public void testThis(int x){
+
+    }
+
+    public void testSlotCircle(){
+        int a = 123;
+        {
+            int b = a + 1;
+            System.out.println(b);
+        }
+        int c = a + 2;
     }
 }
