194849018a5fc14bd40086e918d61d18bb07b28a
koujyungenn <513137767@qq.com>
2022-02-09 13:47:21 +0800

    防止丢失

---
+++ com.kjg/src/code/DynamicLinking.java
@@ -0,0 +1,19 @@
+package src.code;
+
+public class DynamicLinking {
+
+    int num = 10;
+
+    public void A(){
+        B();
+    }
+
+    public void B(){
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
