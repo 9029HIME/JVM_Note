5bef983a1693a2e9437044dd8102d5f0c70a258f
koujyungenn <513137767@qq.com>
2022-02-15 17:05:31 +0800

    虚拟机栈暂告一段落

---
+++ com.kjg/src/code/override/Liquor.java
@@ -0,0 +1,25 @@
+package src.code.override;
+
+public class Liquor {
+    public void drink(){
+        System.out.println("drink normal liquor");
+    }
+
+    public static void main(String[] args) {
+        Liquor liquor = new Beer();
+        liquor.drink();
+    }
+
+    public static void invoke(Liquor liquor){
+        liquor.drink();
+    }
+
+}
+
+class Beer extends Liquor{
+
+    public void drink(){
+        System.out.println("Nice Beer!!!!");
+    }
+}
+
