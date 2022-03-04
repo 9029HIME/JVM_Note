7e106c8968076d7049d13803fd7ae75554c3090d
koujyungenn <513137767@qq.com>
2022-03-04 13:25:37 +0800

    对象内存布局

---
+++ com.kjg/src/code/objectt/Customer.java
@@ -0,0 +1,20 @@
+package src.code.objectt;
+
+public class Customer{
+    int id = 1001;
+    String name;
+    Account acct;
+
+    {
+        name = "匿名客户";
+    }
+    public Customer(){
+        acct = new Account();
+    }
+    public static void main(String[] args) {
+        Customer cust = new Customer();
+    }
+}
+class Account{
+
+}
