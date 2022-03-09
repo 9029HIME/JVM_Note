ff9bf98dbb2d3d36125ce38232f557ae287124c5
koujyungenn <513137767@qq.com>
2022-03-09 21:42:24 +0800

    执行引擎

---
+++ com.kjg/src/code/JitType.java
@@ -0,0 +1,30 @@
+package src.code;
+
+public class JitType {
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+
+        testPrimeNumber(100000000);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("花费的时间为：" + (end - start));
+
+    }
+
+    public static void testPrimeNumber(int count){
+        for (int i = 0; i < count; i++) {
+            //计算100以内的质数
+            label:for(int j = 2;j <= 100;j++){
+                for(int k = 2;k <= Math.sqrt(j);k++){
+                    if(j % k == 0){
+                        continue label;
+                    }
+                }
+                //System.out.println(j);
+            }
+
+        }
+    }
+}
